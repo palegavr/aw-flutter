3c7602fa861a511e18fb4856081c416aab3ca0a0
palegavr <palegavr@gmail.com>
2025-07-25 13:57:16 +0300

    Created EmployeeDto, EmployeeRateDto, get method with mock data in EmployeeService

---
+++ lib/features/employee/application/employee_service.dart
@@ -0,0 +1,102 @@
+import 'package:aw_flutter/features/employee/data/dtos/employee.dart';
+
+class EmployeeService {
+  final List<EmployeeDto> mockEmployees = [
+    EmployeeDto(
+      id: 'emp-001',
+      firstName: 'Ivan',
+      middleName: 'Petrovych',
+      lastName: 'Shevchenko',
+      rank: EmployeeRank.professor,
+      rates: [
+        EmployeeRateDto(
+          rate: 1.0,
+          dateStart: DateTime(2020, 9, 1),
+          dateEnd: DateTime(2023, 8, 31),
+        ),
+      ],
+    ),
+    EmployeeDto(
+      id: 'emp-002',
+      firstName: 'Olena',
+      middleName: 'Andriyivna',
+      lastName: 'Kovalchuk',
+      rank: EmployeeRank.lecturer,
+      rates: [
+        EmployeeRateDto(
+          rate: 0.75,
+          dateStart: DateTime(2021, 2, 15),
+          dateEnd: DateTime(2022, 12, 31),
+        ),
+        EmployeeRateDto(
+          rate: 1.0,
+          dateStart: DateTime(2023, 1, 1),
+          dateEnd: DateTime(2024, 12, 31),
+        ),
+      ],
+    ),
+    EmployeeDto(
+      id: 'emp-003',
+      firstName: 'Taras',
+      middleName: 'Mykolayovych',
+      lastName: 'Melnyk',
+      rank: EmployeeRank.associate,
+      rates: [
+        EmployeeRateDto(
+          rate: 0.5,
+          dateStart: DateTime(2019, 5, 1),
+          dateEnd: DateTime(2020, 5, 1),
+        ),
+        EmployeeRateDto(
+          rate: 0.75,
+          dateStart: DateTime(2020, 5, 2),
+          dateEnd: DateTime(2022, 5, 1),
+        ),
+        EmployeeRateDto(
+          rate: 1.0,
+          dateStart: DateTime(2022, 5, 2),
+          dateEnd: DateTime(2025, 5, 1),
+        ),
+      ],
+    ),
+    EmployeeDto(
+      id: 'emp-004',
+      firstName: 'Nadiia',
+      middleName: 'Volodymyrivna',
+      lastName: 'Tkachenko',
+      rank: EmployeeRank.assistant,
+      rates: [
+        EmployeeRateDto(
+          rate: 0.5,
+          dateStart: DateTime(2022, 1, 10),
+          dateEnd: DateTime(2023, 1, 9),
+        ),
+      ],
+    ),
+    EmployeeDto(
+      id: 'emp-005',
+      firstName: 'Yurii',
+      middleName: 'Stepanovych',
+      lastName: 'Bondarenko',
+      rank: EmployeeRank.head,
+      rates: [
+        EmployeeRateDto(
+          rate: 1.0,
+          dateStart: DateTime(2018, 9, 1),
+          dateEnd: DateTime(2025, 8, 31),
+        ),
+      ],
+    ),
+  ];
+
+
+
+  void createEmployee() async {
+    // todo
+    throw UnimplementedError();
+  }
+
+  Future<List<EmployeeDto>> getEmployees() async {
+    return mockEmployees;
+  }
+}
---
+++ lib/features/employee/data/dtos/employee.dart
@@ -0,0 +1,35 @@
+enum EmployeeRank { head, professor, associate, lecturer, assistant }
+
+class EmployeeDto {
+  final String id;
+  final String firstName;
+  final String middleName;
+  final String lastName;
+  final EmployeeRank rank;
+  final List<EmployeeRateDto> rates;
+
+  EmployeeDto({
+    required this.id,
+    required this.firstName,
+    required this.middleName,
+    required this.lastName,
+    required this.rank,
+    required this.rates,
+  });
+}
+
+class EmployeeRateDto {
+  final double rate; // Ставка, обычно 1.00
+
+  // Если сотрудник работает по данной ставке С определенной даты
+  final DateTime dateStart;
+
+  // Если сотрудник работает по данной ставке ДО определенной даты
+  final DateTime dateEnd;
+
+  EmployeeRateDto({
+    required this.rate,
+    required this.dateStart,
+    required this.dateEnd,
+  });
+}
