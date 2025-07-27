57de63a13ea35da91b881cf29f7cc39005bdc210
palegavr <palegavr@gmail.com>
2025-07-27 10:53:23 +0300

    Refactoring InputHours in workload_distribution feature

--- lib/features/workload_distribution/data/workload_input_model.dart
+++ lib/features/workload_distribution/data/workload_input_model.dart
@@ -4,37 +4,43 @@ enum WlType { common, selective }
 
 enum WlDegree { bachelor, master, doctor }
 
-class WlInputHours {
-  final int? lectures;
-  final int? practices;
-  final int? labs;
-  final int? exams;
-  final int? examConsults;
-  final int? tests;
-  final int? qualificationWorks;
-  final int? workingPractices;
-  final int? examCommittee;
-  final int? reviews;
-  final int? teachingPractices;
-  final int? currentConsults;
-  final int? individualWorks;
-  final int? courseWorks;
+class InputHours {
+  final double? lecturesPlanned; // Лекції по плану
+  final double? lecturesTotal; // Лекції всього
+  final double? practicesPlanned; // Практичні по плану
+  final double? practicesTotal; // Практичні всього
+  final double? labsPlanned; // Лабораторні по плану
+  final double? labsTotal; // Лабораторні всього
+  final double? exams; // Екзамени
+  final double? examConsults; // Консультації перед екзаменом
+  final double? tests; // Заліки
+  final double? qualificationWorks; // Кваліфікаційні роботи
+  final double? certificationExams; // Атестаційний екзамен
+  final double? productionPractices; // Виробнича практика
+  final double? teachingPractices; // Навчальна практика
+  final double? currentConsults; // Поточні консультації
+  final double? individualWorks; // Індивідуальні завдання
+  final double? courseWorks; // Курсові роботи
+  final double? postgraduateExams; // Екзамени для аспірантів
 
-  const WlInputHours({
-    this.lectures,
-    this.practices,
-    this.labs,
-    this.exams,
-    this.examConsults,
-    this.tests,
-    this.qualificationWorks,
-    this.workingPractices,
-    this.examCommittee,
-    this.reviews,
-    this.teachingPractices,
-    this.currentConsults,
-    this.individualWorks,
-    this.courseWorks,
+  InputHours({
+    required this.lecturesPlanned,
+    required this.lecturesTotal,
+    required this.practicesPlanned,
+    required this.practicesTotal,
+    required this.labsPlanned,
+    required this.labsTotal,
+    required this.exams,
+    required this.examConsults,
+    required this.tests,
+    required this.qualificationWorks,
+    required this.certificationExams,
+    required this.productionPractices,
+    required this.teachingPractices,
+    required this.currentConsults,
+    required this.individualWorks,
+    required this.courseWorks,
+    required this.postgraduateExams,
   });
 }
 
@@ -51,7 +57,7 @@ class WorkloadInputModel {
   final int? groups;
   final int? subgroups;
 
-  final WlInputHours hours;
+  final InputHours hours;
 
   const WorkloadInputModel(
     this.name, {
